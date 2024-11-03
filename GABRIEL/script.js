f978b704c1649fb13a6f917c603404ed822206f3
Gabriel Oliveira <gabrieloliverasantos162@gmail.com>
2024-11-03 19:39:03 -0300

    gkjyui6

---
+++ GABRIEL/script.js
@@ -0,0 +1,9 @@
+const typed = new Typed('#typed-Text', {
+    strings:["DOCTOR WEB"],
+    typeSpeed: 100,//velocidade que sera digitado nosso texto
+    backSpeed: 25,//velocidade de retorno
+    loop: true,//faz com que o texto fique em execução infinita
+    showCursor: false,//estamos removendo o cursor padrao do HTML
+    fadeOut: true,//ADICIONA UMA TRANSIÇÂO (OPCIONAL)
+    
+});
