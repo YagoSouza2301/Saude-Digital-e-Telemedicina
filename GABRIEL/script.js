96671d857e973ce4693d15704e978cb9d7433bf9
Gabriel Oliveira <gabrieloliverasantos162@gmail.com>
2024-11-06 08:50:27 -0300

    aesf

--- GABRIEL/script.js
+++ GABRIEL/script.js
@@ -1,9 +1,8 @@
 const typed = new Typed('#typed-Text', {
     strings:["DOCTOR WEB"],
-    typeSpeed: 100,//velocidade que sera digitado nosso texto
-    backSpeed: 25,//velocidade de retorno
+    typeSpeed: 400,//velocidade que sera digitado nosso texto
+    backSpeed: 200,//velocidade de retorno
     loop: true,//faz com que o texto fique em execução infinita
     showCursor: false,//estamos removendo o cursor padrao do HTML
-    fadeOut: true,//ADICIONA UMA TRANSIÇÂO (OPCIONAL)
-    
+    fadeOut: false,//ADICIONA UMA TRANSIÇÂO (OPCIONAL)
 });
