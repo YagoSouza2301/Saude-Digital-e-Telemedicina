c78866750bdd39fec3a393aec29a96bb9bb91d37
Gabriel Oliveira <gabrieloliverasantos162@gmail.com>
2024-11-07 09:14:55 -0300

    criei 2 sessoes e cards

--- GABRIEL/script.js
+++ GABRIEL/script.js
@@ -1,8 +1,39 @@
-const typed = new Typed('#typed-Text', {
-    strings:["DOCTOR WEB"],
-    typeSpeed: 400,//velocidade que sera digitado nosso texto
-    backSpeed: 200,//velocidade de retorno
-    loop: true,//faz com que o texto fique em execução infinita
-    showCursor: false,//estamos removendo o cursor padrao do HTML
-    fadeOut: false,//ADICIONA UMA TRANSIÇÂO (OPCIONAL)
-});
+ScrollReveal().reveal('.div-dad1',{
+    duration: 1000, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'left', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+ScrollReveal().reveal('.div-dad2',{
+    duration: 1500, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'right', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+ScrollReveal().reveal('.div3',{
+    duration: 2000, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'right', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+
+
+$(document).ready(function() {
+
+    // Seleciona o conteiner . carousel e aplica a funçao slick()
+    // que ativa o carrossel com as configurações abaixo
+    $('.carousel').slick({
+
+        //slidesToShow: numero de cards exibidos ao mesmo tempo no carrossel
+        slidesToShow: 1, // exibe 3 cards ao mesmo tempo
+
+        //slidesToScroll: define quantos cards serao deslizados ao mover o carrossel
+        slidesToScroll: 1, // Move 1 car por vez ao deslizar
+
+        // infinite: quando true, o carrossel retona ao inicio automaticamente
+        Infinite: true, //mantem o carrossel em rotação continua
+
+        //dots: quando true, ativa os pontos de navegação abaixo do carrossel
+        dots: true, // adiciona pontos de navegação para os usuarios
+
+        // arrows: exibe setas de navegação nas laterais do carrossel
+        arrows: true, // ativa as setas para navegar entre os cards
+    })
+})
