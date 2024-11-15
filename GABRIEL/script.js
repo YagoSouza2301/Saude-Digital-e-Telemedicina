e16c45d8d2055408ecfa8908d09ce9cc053f9249
Gabriel Oliveira <gabrieloliverasantos162@gmail.com>
2024-11-14 21:01:04 -0300

    Update script.js

--- GABRIEL/script.js
+++ GABRIEL/script.js
@@ -13,6 +13,11 @@ ScrollReveal().reveal('.div3',{
     origin: 'right', // origem do efeito: o conteudo virá da direita para a esquerda
     distance : '50px', // distancia que o elemento "viaja" antes de aparecer
 })
+ScrollReveal().reveal('.carousel',{
+    duration: 3000, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'bottom', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
 
 
 $(document).ready(function() {
