b4c237669ca2879a4d6afe43435273bb5373821d
Gabriel Oliveira <gabrieloliverasantos162@gmail.com>
2024-11-24 14:31:44 -0300

    colocada a responsividade e trocado o header e o footer

--- GABRIEL/script.js
+++ GABRIEL/script.js
@@ -60,4 +60,16 @@ function moveCarousel() {
 //define um intervalo para mover o carrossel automaticamente a cada 3 segundos
 setInterval(moveCarousel, 10000);
 
+// VERSÃO MOBILE
+//Aqui  adicionamos um evento de escutar o click do Mouse sobre o ícone de Hamburger.
+hamburgerButton.addEventListener("click", function () {
+    mobileMenu.classList.add("flex");
+    // Criamos uma função que a ouvir o click do Mouse, ele adicione uma Class, que está fazendo mossa caixa (DIV) aparecer, ou seja, ficar visivel
+});
+
+//Criamos uma função que a o ouvir o click do mouse, ele REMOVA uma Class, que está fazendo mossa caixa (Div) desaparecer, ou seja, ficar invisivel.
+closeButton.addEventListener("click", function () {
+    mobileMenu.classList.remove("flex");
+});
+
 
